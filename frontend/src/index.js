d63705ef9df98fd2fb1c7902dbe85928ce4b03a2
Pratikmhrzn1 <mhrznpratik11@gmail.com>
2024-11-27 16:19:35 +0545

    Last commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,8 @@ import "./assets/styles/index.css";
 import App from "./App";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import { Provider } from "react-redux";
+import store from "./store";
 import reportWebVitals from "./reportWebVitals";
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,7 +26,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 reportWebVitals();
